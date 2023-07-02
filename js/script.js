23ad800812632e760749c67d9a917d250f6b334d
vshevchuk <shevchukvitalik@bk.ru>
2023-07-02 11:46:12 +0300

    Sections observer

--- js/script.js
+++ js/script.js
@@ -21,12 +21,12 @@
 
 const menuHeight = document.querySelector(".header-top").clientHeight;
 
-const menuElementLinks = document.querySelectorAll(".menu-element__link");
-menuElementLinks.forEach(element => {
+/* Active link after click */
+document.querySelectorAll(".menu-element__link").forEach(element => {
     element.addEventListener("click", function (e) {
         e.preventDefault();
 
-        if (this.classList.contains('active')) {
+        if (hasClass(this, 'active')) {
             return;
         }
 
@@ -41,7 +41,42 @@ menuElementLinks.forEach(element => {
             top: hrefElement.offsetTop - paddingTop - menuHeight
         });
 
-        document.querySelector(".menu-element__link.active").classList.remove('active');
-        this.classList.add('active');
+        changeActiveMenuElement(this);
     });
 });
+
+/* Active link on scroll to section */
+const options = {
+    threshold: 0.3
+}
+const callback = function(entries) {
+    entries.forEach(entry => {
+        const sectionMenuLink = document.querySelector(`.menu-element__link[href='#${entry.target.id}']`);
+        if (entry.isIntersecting && !hasClass(sectionMenuLink, 'active')) {
+            changeActiveMenuElement(sectionMenuLink);
+        }
+    });
+};
+const observer = new IntersectionObserver(callback, options);
+document.querySelectorAll(".observer-section").forEach(element => {
+    observer.observe(element);
+});
+
+// Общие функции
+/**
+ * Меняет активный элемент в пунктах меню
+ * @param element Ссылка в меню
+ */
+function changeActiveMenuElement(element) {
+    document.querySelector(".menu-element__link.active").classList.remove('active');
+    element.classList.add('active');
+}
+
+/**
+ * Проверяет наличие класса у элемента
+ * @param element
+ * @param className
+ */
+function hasClass(element, className) {
+    return element.classList.contains(className);
+}
